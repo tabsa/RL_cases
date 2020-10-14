b06467c25f1606294e4b739013a519d439408b63
Tiago Sousa <tsousa@win.dtu.dk>
2020-10-15 00:23:13 +0200

    Informs simulation

--- Multi_armed_bandit/energy_p2p_market.py
+++ Multi_armed_bandit/energy_p2p_market.py
@@ -32,14 +32,16 @@ from p2p_as_mad_class import trading_env, trading_agent
 #%% Informs simulation
 no_trials = 40
 no_agents = 15
-no_episodes = 10
+no_episodes = 100
+no_RL_agents = 3
 target_sample = np.zeros(no_episodes)
 target_bounds = np.array([3, 15])
 # Plot parameters
 cmap = plt.get_cmap("tab10", 15)
 sns.set_style("whitegrid")
 
-# For-loop per simulation
+rd_score = np.zeros((no_RL_agents,no_episodes)) # Reward score per simulation
+# For-loop per simulation [1,...,no_episodes]
 for e in range(no_episodes):
     target_sample[e] = np.random.uniform(low=target_bounds[0], high=target_bounds[1])
     env = trading_env(no_agents, no_trials, 'offers_input.csv', 'External_sample', target_sample[e])
@@ -48,15 +50,35 @@ for e in range(no_episodes):
     # Simulate the Random-policy
     rd_agent = trading_agent(env, 'Random_policy')
     env.run(rd_agent)
-    rd_agent.plot_action_choice(cmap, f'Random policy across {no_trials} trials of episode {e}')
+    #rd_agent.plot_action_choice(cmap, f'Random policy across {no_trials} trials of episode {e}')
+    rd_score[0, e] = rd_agent.total_reward
     # Simulate the eGreedy-policy
     eg_agent = trading_agent(env, 'e-greedy_policy', time_learning=10, e_greedy=0.25)  # Agent using the e-Greedy policy
     env.run(eg_agent)
-    eg_agent.plot_action_choice(cmap, f'e-Greedy policy across {no_trials} trials of episode {e}')
+    #eg_agent.plot_action_choice(cmap, f'e-Greedy policy across {no_trials} trials of episode {e}')
+    rd_score[1, e] = eg_agent.total_reward
     # Simulate the Thompson-Sampler-policy
     ts_agent = trading_agent(env, 'Thompson_Sampler_policy')
     env.run(ts_agent)
-    ts_agent.plot_action_choice(cmap, f'Thompson-Sampler policy across {no_trials} steps of episode {e}')
+    #ts_agent.plot_action_choice(cmap, f'Thompson-Sampler policy across {no_trials} steps of episode {e}')
+    rd_score[2, e] = ts_agent.total_reward
+
+print(f'All {no_episodes} Simulations Done')
+# Plot comparing the 3 policies implemented to the MAD problem
+plt.figure(figsize=(10,7))
+x = np.arange(0, no_episodes)
+plt_label = ['Random', 'e-Greedy', 'Thompson']
+plt_marker = ['o', '*', '^']
+for i in range(no_RL_agents):
+    plt.plot(x,rd_score[i,:], label=plt_label[i], marker=plt_marker[i], linestyle='--')
+# Legend and labels of the plot
+plt.legend(fontsize=16)
+plt.ylabel('Total Reward', fontsize=16)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+plt.xlabel('Episode #', fontsize=16)
+# plt.title(f'Comparison of policies in MAD problem across {n_tests} simulations', fontsize=20)
+plt.show()
 
 #%% Script - Using the p2p_class_code
 # no_hours = 100
@@ -72,78 +94,3 @@ for e in range(no_episodes):
 # env = p2p_env(p2p_agents, no_time, time_step)
 # #rd_agent = p2p_RL_agent(env, 'Random_policy')
 # env.set_simulation()
-
-#%% Input of the Multi-Armed Bandit (MAD) problem
-#
-# # Agent performance
-# rd_agent = Agent(env, 'Random_policy') # Agent using the Random policy
-# eg_agent = Agent(env, 'e-greedy_policy', time_learning=500, e_greedy=0.1) # Agent using the e-Greedy policy
-# ts_agent = Agent(env, 'Thompson_Sampler_policy') # Agent using the Thompson-Sampler policy
-# # Simulation with rd_agent
-# env.run(rd_agent)
-# rd_agent.plot_action_choice(cmap, f'Random policy across {n_trials} steps')
-# # Simulation with eg_agent
-# env.run(eg_agent)
-# eg_agent.plot_action_choice(cmap, f'e-Greedy policy across {n_trials} steps')
-# # Simulation with ts_agent
-# env.run(ts_agent)
-# ts_agent.plot_action_choice(cmap, f'Thompson-Sampler policy across {n_trials} steps')
-#
-# # Plot regret over trial (opportunity cost of selecting a better action)
-# # Shows the results for the agents of the last test - n_tests = 10
-# plt.figure(figsize=(10,7))
-# # Subplot 1
-# plt.subplot(211) # 2 rows and 1 column
-# plt.plot(np.cumsum(1-rd_agent.theta_regret_t), label='random') # Agent with random policy
-# plt.plot(np.cumsum(1-eg_agent.theta_regret_t), label='e-greedy') # Agent with e-Greedy policy
-# plt.plot(np.cumsum(1-ts_agent.theta_regret_t), label='thompson') # Agent with Thompson-Sampler policy
-# plt.xlabel('no Trials #', fontsize=16)
-# plt.ylabel('Regret', fontsize=16)
-# plt.title(f'Cumulative regret across {n_trials} steps')
-# plt.legend()
-# # Subplot 2
-# plt.subplot(212)
-# plt.plot(1-rd_agent.theta_regret_t, label='random')
-# plt.plot(1-eg_agent.theta_regret_t, label='e-greedy')
-# plt.plot(1-ts_agent.theta_regret_t, label='thompson')
-# plt.xlabel('no Trials #', fontsize=16)
-# plt.ylabel('Regret', fontsize=16)
-# plt.title(f'Regret per time t (1,...,{n_trials})')
-# plt.legend()
-# plt.show()
-#
-# # Simulation part
-# n_tests = 10
-# rd_score = np.zeros((n_agents,n_tests)) # Reward score per simulation
-# # For-loop i from [1,...,n_tests]
-# for i in range(n_tests):
-#     print(f'Simulation {i+1}')
-#     # Agent using the Random policy
-#     rd_agent = Agent(env, 'Random_policy')
-#     env.run(rd_agent)
-#     rd_score[0,i] = rd_agent.total_reward
-#     # Agent using the e-Greedy policy
-#     eg_agent = Agent(env, 'e-greedy_policy', time_learning=500, e_greedy=0.1)
-#     env.run(eg_agent)
-#     rd_score[1,i] = eg_agent.total_reward
-#     # Agent using the Thompson-Sampler policy
-#     ts_agent = Agent(env, 'Thompson_Sampler_policy')
-#     env.run(ts_agent)
-#     rd_score[2,i] = ts_agent.total_reward
-#
-# print(f'All {n_tests} Simulations Done')
-# # Plot comparing the 3 policies implemented to the MAD problem
-# plt.figure(figsize=(10,7))
-# x = np.arange(0, n_tests)
-# plt_label = ['random', 'e-greedy', 'thompson']
-# plt_marker = ['o', '*', '^']
-# for i in range(n_agents):
-#     plt.plot(x,rd_score[i,:], label=plt_label[i], marker=plt_marker[i], linestyle='--')
-# # Legend and labels of the plot
-# plt.legend(fontsize=16)
-# plt.ylabel('Total Reward', fontsize=16)
-# plt.xticks(fontsize=16)
-# plt.yticks(fontsize=16)
-# plt.xlabel('Simulation #', fontsize=16)
-# plt.title(f'Comparison of policies in MAD problem across {n_tests} simulations', fontsize=20)
-# plt.show()
